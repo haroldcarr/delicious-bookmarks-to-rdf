2c3b16112afe1b964fdb23f8e749440aa7b3dff2
Harold Carr <harold.carr@gmail.com>
2017-07-24 14:33:17 -0700

    Use UTF8.   Filters to clean up my tags.

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,13 +6,16 @@ module Lib where
 -- does not handle multi-line notes in <DD>
 
 {-
+parseDelicious "test/test-data/delicious-2017-07-14.html"
 deliciousToRdf "test/test-data/delicious-2017-07-14.html" "/tmp/delicious-2017-07-14.ttl"
 -}
 
-import           ClassyPrelude   as CP hiding (many, try, (<|>))
-import           Data.List.Split (splitOn)
-import           System.IO       as IO (IOMode (WriteMode), hPutStr, hPutStrLn,
-                                        readFile, withFile)
+import           ClassyPrelude         as CP hiding (many, try, (<|>))
+import           Data.ByteString       as BS hiding (hPutStrLn)
+import           Data.ByteString.Char8 as BS (hPutStrLn)
+import           Data.Text             as T (pack, replace, splitOn, strip)
+import           Data.Text.Encoding    as T (decodeUtf8, encodeUtf8)
+import           System.IO             as IO (IOMode (WriteMode), withFile)
 import           Text.Parsec
 
 ------------------------------------------------------------------------------
@@ -26,70 +29,244 @@ deliciousToRdf i o = do
 
 data Bookmark
   = Bookmark
-  { href    :: String
-  , addDate :: String
-  , private :: String
-  , tags    :: [String]
-  , title   :: String
-  , notes   :: String
+  { href    :: Text
+  , addDate :: Text
+  , private :: Text
+  , tags    :: [Text]
+  , title   :: Text
+  , notes   :: Text
   }
   deriving (Show)
 
 ------------------------------------------------------------------------------
 -- write RDF
 
-hc :: String
+hc :: Text
 hc = "@prefix hc: <http://openhc.org/syntax-ns#> ."
 
 writeRdf :: FilePath -> [Bookmark] -> IO ()
 writeRdf o bs =
   withFile o WriteMode $ \h -> do
-    hPutStrLn h hc
+    BS.hPutStrLn h (T.encodeUtf8 hc)
     forM_ bs $ writeBookmark h
 
 writeBookmark :: Handle -> Bookmark -> IO ()
 writeBookmark h b = do
-  hPutStrLn h ""
-  hPutStr h "<"; hPutStr h (href b); hPutStrLn h ">"
+  BS.hPutStrLn h ""
+  hPutStr h "<"; hPutStr h (T.encodeUtf8 (href b)); BS.hPutStrLn h ">"
   writeProperty h "hc:addDate" (addDate b) ";"
   writeProperty h "hc:private" (private b) ";"
   writeTags h (tags b)
   if notes b == "" then
-      writeProperty h "hc:title" (show (title b)) "."
+      writeProperty h "hc:title" (lintTitle (title b)) "."
   else do
-      writeProperty h "hc:title" (show (title b)) ";"
-      writeProperty h "hc:notes" (show (notes b)) "."
+      writeProperty h "hc:title" (lintTitle (title b)) ";"
+      writeProperty h "hc:notes" (notes b) "."
   return ()
 
+-- some titles have " (meaning inches) in them
+lintTitle :: Text -> Text
+lintTitle = T.replace "\"" ""
+
 -- some of my delicious files has "tag,,tag" - clean that up
-writeTags :: Handle -> [String] -> IO ()
+writeTags :: Handle -> [Text] -> IO ()
 writeTags h ts =
   forM_ ts $ \t ->
     when (t /= "")
-      (writeProperty h "hc:tag" (show t) ";")
+      (writeProperty h "hc:tag" (lintTag t) ";")
 
-writeProperty :: Handle -> String -> String -> String -> IO ()
+lintTag :: Text -> Text
+lintTag t =
+  case T.strip t of
+    "401K"          -> "401k"
+    "aes"           -> "AES"
+    "aeson"         -> "Aeson"
+    "agda"          -> "Agda"
+    "akka"          -> "Akka"
+    "amazon"        -> "Amazon"
+    "amazon_images" -> "Amazon_images"
+    "android"       -> "Android"
+    "android_app"   -> "Android"
+    "argonaut"      -> "Argonaut"
+    "asm"           -> "ASM"
+    "awk"           -> "AWK"
+    "bandcamp"      -> "Bandcamp"
+    "beamer"        -> "Beamer"
+    "bitcoin"       -> "Bitcoin"
+    "calais"        -> "Calais"
+    "casa_de_piedra_con_con_chile"
+                    -> "Casa_de_Piedra_Con_Con_Chile"
+    "chile"         -> "Chile"
+    "cider"         -> "Cider"
+    "clojure"       -> "Clojure"
+    "typed_clojure" -> "Clojure_typed"
+    "cloud_haskell" -> "Cloud_Haskell"
+    "coen_brother"  -> "Coen_brothers"
+    "coffeescript"  -> "CoffeeScript"
+    "colorado"      -> "Colorado"
+    "conkeror"      -> "Conkeror"
+    "coq"           -> "Coq"
+    "cpntools"      -> "CpnTools"
+    "css"           -> "CSS"
+    "datalet"       -> "datlet"
+    "ditaa"         -> "Ditaa"
+    "dns"           -> "DNS"
+    "docbook"       -> "DocBook"
+    "docker"        -> "Docker"
+    "dropbox"       -> "Dropbox"
+    "dropbox_alternative"
+                    -> "Dropbox_alternative"
+    "duckduckgo"    -> "DuckDuckGo"
+    "elcim"         -> "Elcim"
+    "elisp"         -> "Elisp"
+    "elm"           -> "Elm"
+    "emacs"         -> "Emacs"
+    "spacemacs"     -> "Emacs_spacemacs"
+    "erlang"        -> "Erlang"
+    "ethereum"      -> "Ethereum"
+    "frege"         -> "Frege"
+    "ftp"           -> "FTP"
+    "f#"            -> "F-sharp"
+    "F#"            -> "F-sharp"
+    "f_sharp"       -> "F-sharp"
+    "f*"            -> "F-star"
+    "facebook"      -> "Facebook"
+    "facebook_connect"
+                    -> "Facebook_connect"
+    "facebook_friend_inviter"
+                    -> "Facebook_friend_inviter"
+    "fay"           -> "Fay"
+    "fax"           -> "FAX"
+    "firefox_extensions"
+                    -> "Firefox_extensions"
+    "frp"           -> "FRP"
+    "galois"        -> "Galois"
+    "gasprice"      -> "gas_price"
+    "ghcjs"         -> "GHCJS"
+    "ghc"           -> "GHC"
+    "git"           -> "Git"
+    "git_annex"     -> "Git_Annex"
+    "github"        -> "Github"
+    "gmail"         -> "Gmail"
+    "gnus"          -> "Gnus"
+    "google"        -> "Google"
+    "google_analytics"-> "Google_analytics"
+    "google_calendar"-> "Google_calendar"
+    "google_maps"   -> "Google_maps"
+    "google_refine" -> "Google_refine"
+    "google_wave"   -> "Google_wave"
+    "googlereader"  -> "Google_reader"
+    "graphviz"      -> "GraphViz"
+    "haddocks"      -> "Haddocks"
+    "hakyll"        -> "Hakyll"
+    "haskel "       -> "Haskell"
+    "Haskel "       -> "Haskell"
+    "haskel"        -> "Haskell"
+    "haskell"       -> "Haskell"
+    "haskell_example"
+                    -> "Haskell_example"
+    "haskell_extensions"
+                    -> "Haskell_extensions"
+    "haskell_job"   -> "Haskell_job"
+    "haskell_mode"  -> "Haskell_mode"
+    "haskell-mode"  -> "Haskell-mode"
+    "haskell_pipes" -> "Haskell_pipes"
+    "haskell_reflection"
+                    -> "Haskell_reflection"
+    "haskell_sandbox"
+                    -> "Haskell_sandbox"
+    "haskell_stack"
+                    -> "Haskell_stack"
+    "stackage"      -> "Haskell_stackage"
+    "hackage"       -> "Haskell_hackage"
+    "template_haskell" -> "Haskell_template"
+    "hawaii"        -> "Hawaii"
+    "haxe"          -> "Haxe"
+    "html"          -> "HTML"
+    "html5"         -> "HTML5"
+    "http"          -> "HTTP"
+    "httpd"         -> "HTTPd"
+    "idris"         -> "Idris"
+    "isabelle"      -> "Isabelle"
+    "java"          -> "Java"
+    "javadoc"       -> "Javadoc"
+    "java_generics" -> "Java_generics"
+    "java_lambda"   -> "Java_lambda"
+    "java_reflection"-> "Java_reflection"
+    "java_snow_leopard"   -> "Java_snow_leopard"
+    "javascript"    -> "Javascript"
+    "javascript_MVC_framework"    -> "Javascript_MVC_framework"
+    "javaslang"     -> "Javaslang"
+    "jini"          -> "Jini"
+    "jitsi"         -> "Jitsi"
+    "json"          -> "JSON"
+    "jvm"           -> "JVM"
+    "kadena"        -> "Kadena"
+    "lambda_conf"   -> "LambdaConf"
+    "latex"         -> "Latex"
+    "ledger"        -> "Ledger"
+    "linux"         -> "Linux"
+    "liquid_haskell"-> "Liquid_Haskell"
+    "lisp"          -> "Lisp"
+    "little_snitch" -> "Little_Snitch"
+    "llvm"          -> "LLVM"
+    "microsoft"     -> "Microsoft"
+    "ml"            -> "ML"
+    "nixos"         -> "NixOS"
+    "nix"           -> "Nix"
+    "ocaml"         -> "OCaml"
+    "paxos"         -> "Paxos"
+    "pdf"           -> "PDF"
+    "purescript"    -> "PureScript"
+    "python"        -> "Python"
+    "quickcheck"    -> "QuickCheck"
+    "redex"         -> "Redex"
+    "racket"        -> "Racket"
+    "recusion"      -> "recursion"
+    "rest"          -> "REST"
+    "roku"          -> "Roku"
+    "ruby"          -> "Ruby"
+    "scala"         -> "Scala"
+    "scalaz"        -> "ScalaZ"
+    "scheme"        -> "Scheme"
+    "sed"           -> "SED"
+    "servant"       -> "Servant"
+    "sicp"          -> "SICP"
+    "skills_matter" -> "Skill_Matter"
+    "skype"         -> "Skype"
+    "slime"         -> "Slime"
+    "smalltalk"     -> "Smalltalk"
+    "tex"           -> "Tex"
+    "tikz"          -> "TikZ"
+    "tla"           -> "TLA"
+    "unix"          -> "Unix"
+    "ur/web"        -> "Ur/Web"
+    "utah"          -> "Utah"
+    "vpn"           -> "VPN"
+    "zotero"        -> "Zotero"
+    x               -> x
+
+writeProperty :: Handle -> Text -> Text -> Text -> IO ()
 writeProperty h p v sep = do
   hPutStr h "    "
-  hPutStr h p
+  hPutStr h (T.encodeUtf8 p)
   hPutStr h " "
-  hPutStr h v
+  hPutStr h "\""; hPutStr h (T.encodeUtf8 v); hPutStr h "\""
   hPutStr h " "
-  hPutStrLn h sep
-
+  BS.hPutStrLn h (T.encodeUtf8 sep)
 
 ------------------------------------------------------------------------------
 -- parse delicious
 
-type Parser = Parsec String ()
+type Parser = Parsec Text ()
 
 parseDelicious :: FilePath -> IO (Either ParseError [Bookmark])
 parseDelicious = parseFromFile delicious
 
 parseFromFile :: Parser a -> FilePath -> IO (Either ParseError a)
 parseFromFile p fname = do
-  i <- IO.readFile fname
-  return (runParser p () fname i)
+  i <- BS.readFile fname
+  return (runParser p () fname (T.decodeUtf8 i))
 
 delicious :: Parser [Bookmark]
 delicious = do
@@ -115,16 +292,16 @@ bookmark = do
   ti <- manyTill anyChar (try (string "</A>"))
   spaces
   n <- bookmarkNote <|> return ""
-  return $ Bookmark h a p ts ti n
+  return $ Bookmark h a p ts (T.pack ti) n
 
-bookmarkNote :: Parser String
+bookmarkNote :: Parser Text
 bookmarkNote = do
   void $ try (string "<DD>")
-  manyTill anyChar endOfLine
+  fmap T.pack (manyTill anyChar endOfLine)
 
-stringContents :: Parser String
+stringContents :: Parser Text
 stringContents =
-  manyTill anyChar (try (char '"'))
+  fmap T.pack (manyTill anyChar (try (char '"')))
 
 delHeader :: Parser ()
 delHeader = do
@@ -145,7 +322,7 @@ delHeader = do
   void $ string "<DL><p>"
   void   endOfLine
 
-theEnd :: Parser String
+theEnd :: Parser Text
 theEnd = do
   void $ many anyChar
   eof
